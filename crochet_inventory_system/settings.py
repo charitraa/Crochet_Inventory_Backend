f86d8373fa9cc21b823aa4ab7adbffa4ddfc35d4
charitra shrestha <stharabi9862187405@gmail.com>
2025-03-25 18:21:45 +0545

    ready for production

--- crochet_inventory_system/settings.py
+++ crochet_inventory_system/settings.py
@@ -156,7 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "https://https://takris-crochet.vercel.app/",
     "https://charitra.pythonanywhere.com",
 ]
 
