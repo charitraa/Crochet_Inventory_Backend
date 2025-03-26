255836bd7514311313aa596c64976ed881ed927c
charitra shrestha <stharabi9862187405@gmail.com>
2025-03-26 08:29:18 +0545

    update the cros ip

--- crochet_inventory_system/settings.py
+++ crochet_inventory_system/settings.py
@@ -156,7 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://https://takris-crochet.vercel.app/",
+    "https://charitraa.github.io/Crochet-Inventory-System/",
     "https://charitra.pythonanywhere.com",
 ]
 
